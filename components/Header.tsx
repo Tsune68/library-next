74e40b99b1e3cc3ad2595dc05cefb40e7e4ea0bf
恒川泰智 <tsunekawataichi@Hajimari-TsunekawaTaichi.local>
2024-03-22 09:37:03 +0900

    bookモデルの追加等

---
+++ components/Header.tsx
@@ -0,0 +1,45 @@
+import Link from "next/link";
+import React from "react";
+import { signIn, signOut, useSession } from "next-auth/react";
+
+const Navbar = () => {
+  const { data: session, status } = useSession();
+  return (
+    <header className="bg-gray-700 p-4 text-white">
+      <div className="container mx-auto flex justify-between items-center">
+        <h1 className="font-semibold text-xl">
+          <Link href="/" className="text-2xl">
+            hajimari
+          </Link>
+        </h1>
+        <nav>
+          <ul className="flex space-x-4">
+            {status === "authenticated" ? (
+              <>
+                <Link href={`/profile/${session?.user?.name}`} className="flex items-center">
+                  <img
+                    src={session.user.image}
+                    className="rounded-full w-10 h-10"
+                    alt="丸いアイコン"
+                  />
+                  <p className="ml-4">{session.user?.name}</p>
+                </Link>
+              </>
+            ) : (
+              <>
+                <Link
+                  href="auth/signin"
+                  className="bg-white text-gray-900 py-2 px-3 rounded-lg font-medium"
+                >
+                  ログイン
+                </Link>
+              </>
+            )}
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
