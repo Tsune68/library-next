c7ec3a3a44a1cb38fbd64d7864116a622f6d4fad
恒川泰智 <tsunekawataichi@Hajimari-TsunekawaTaichi.local>
2024-05-13 23:47:52 +0900

    add:貸し出し履歴用のテーブル作成

---
+++ prisma/migrations/20240511080846_add_rental_history_table/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "RentalHistory" (
+    "id" SERIAL NOT NULL,
+    "userId" TEXT NOT NULL,
+    "bookId" TEXT NOT NULL,
+    "borrowedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "returnedAt" TIMESTAMP(3),
+
+    CONSTRAINT "RentalHistory_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "RentalHistory" ADD CONSTRAINT "RentalHistory_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "RentalHistory" ADD CONSTRAINT "RentalHistory_bookId_fkey" FOREIGN KEY ("bookId") REFERENCES "Book"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
